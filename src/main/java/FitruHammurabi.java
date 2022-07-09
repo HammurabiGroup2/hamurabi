fb1d0eb8ddc1c157d38a5ae89058942b807d3069
Fitru <fitru@ZipcodersMBP100.fios-router.home>
2022-07-08 22:09:06 -0400

    First start

---
+++ src/main/java/FitruHammurabi.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FitruHammurabi {
+
+           // must save in a file named Hammurabi.java
+        Random rand = new Random();  // this is an instance variable
+        Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) { // required in every Java program
+            new FitruHammurabi().playGame();
+        }
+
+
+    void playGame() {
+
+
+
+
+
+        // declare local variables here: grain, population, etc.
+        // statements go after the declations
+    }
+
+
+
+}
+
+
+
+
+//other methods go here
+
+
